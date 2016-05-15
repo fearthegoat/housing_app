6e058ca358d640a19d835dac82f052273f6e07ab
Kevin <usacitizen80@hotmail.com>
2016-05-14 22:38:24 -0400

    Initial Commit

---
+++ lib/tasks/sales_fetcher.rb
@@ -0,0 +1,13 @@
+task sales_fetcher: :environment do
+  page = `curl https://www.navyfederal.org/products-services/loans/mortgage/mortgage-rates.php`
+
+  noko_page = Nokogiri::HTML(page)
+
+  term = 30
+
+  rate = noko_page.css('tr:contains("30 Yr Conforming") td:eq(3)').text
+  rate_float = rate.to_f
+  puts "#{rate_float}"
+  Rate.create(initial_rate: rate_float, term: term)
+
+end
