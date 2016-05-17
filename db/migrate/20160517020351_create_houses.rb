c4b14328fa0c4f9be9a2c8b6f01a2927001980f6
Kevin Manuel <kmanuel09@gmail.com>
2016-05-16 22:04:08 -0400

    creates housing model and migration

---
+++ db/migrate/20160517020351_create_houses.rb
@@ -0,0 +1,27 @@
+class CreateHouses < ActiveRecord::Migration
+  def change
+    create_table :houses do |t|
+      t.integer :number
+      t.string :street
+      t.string :street_type
+      t.integer :zip
+      t.string :unit
+      t.string :owner_first_name
+      t.string :owner_last_name
+      t.string :map_number
+      t.integer :land_area
+      t.string :land_use
+      t.integer :book
+      t.integer :page
+      t.integer :sq_ft
+      t.integer :year_built
+      t.string :style
+      t.integer :bedrooms
+      t.integer :full_baths
+      t.integer :half_baths
+      t.integer :fireplaces
+
+      t.timestamps null: false
+    end
+  end
+end
