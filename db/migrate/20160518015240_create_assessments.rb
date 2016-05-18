2bd2bd4bf6a0d8242066f887d65af94f98ac4bab
Kevin Manuel <kmanuel09@gmail.com>
2016-05-17 21:52:56 -0400

    adds assessment model and migration

---
+++ db/migrate/20160518015240_create_assessments.rb
@@ -0,0 +1,12 @@
+class CreateAssessments < ActiveRecord::Migration
+  def change
+    create_table :assessments do |t|
+      t.integer :year
+      t.integer :land
+      t.integer :building
+      t.integer :assessed_total
+
+      t.timestamps null: false
+    end
+  end
+end
